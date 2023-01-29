6f4aadc4959733f42128f827d370ffae17605393
Hank Kim <hank29206880@gmail.com>
2023-01-29 11:31:27 +0900

    Project 페이지 생성

--- client/src/pages/Project.jsx
+++ client/src/pages/Project.jsx
@@ -1,6 +1,16 @@
-import React from "react";
+import { useQuery } from "@apollo/client";
+import { Link, useParams } from "react-router-dom";
+import Spinner from "../components/Spinner";
+import { GET_PROJECT } from "../queries/projectQueries";
 
 function Project() {
+  const { id } = useParams();
+  const { loading, error, data } = useQuery(GET_PROJECT, {
+    variables: { id },
+  });
+  if (loading) return <Spinner />;
+  if (error) return <p>wtf</p>;
+
   return <div>Project</div>;
 }
 
